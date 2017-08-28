cf2b8423e360ee7e2d5f7dace06dfeaea59d8774
NullDev (Shadow) <NL-Dev@yandex.com>
2017-08-28 11:05:03 +0200

    Removed unused "});"

--- WBB_Modal_Link.js
+++ WBB_Modal_Link.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
     $('#adminapp').removeAttr('href');
     $('#adminapp').attr('title','Click to get to the admin application form');
     var modal = document.getElementById('errsyntax'), span = document.getElementById('close1');
-        span.onclick  = function() { modal.style.display = "none"; }
-        window.onclick = function(event) { if (event.target == modal) modal.style.display = "none"; }
-        $('#adminapp' ).click(function (){ modal.style.display = "block"; });
-    });
+    span.onclick  = function() { modal.style.display = "none"; }
+    window.onclick = function(event) { if (event.target == modal) modal.style.display = "none"; }
+    $('#adminapp' ).click(function (){ modal.style.display = "block"; });
 });
